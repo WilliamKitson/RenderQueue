084a586ac37b4792bc9970cbf27827580bcdfefd
BEELZEBLUBBER <43683673+BEELZEBLUBBER@users.noreply.github.com>
2022-07-26 12:10:16 +0100

    ObjectsCountPopTest successes function defined

--- TestSuite/src/domain/objects/count/ObjectsCountPopTest.cpp
+++ TestSuite/src/domain/objects/count/ObjectsCountPopTest.cpp
@@ -1,7 +1,7 @@
 #include "ObjectsCountPopTest.h"
 
 ObjectsCountPopTest::ObjectsCountPopTest()
-	: itterations{ 4 }
+	: unit(), itterations{ 4 }
 {
 }
 
@@ -11,25 +11,28 @@ ObjectsCountPopTest::~ObjectsCountPopTest()
 
 std::string ObjectsCountPopTest::test()
 {
-	RenderQueue::Objects unit;
-
 	for (int i{ 0 }; i < itterations; i++)
 	{
 		unit.push();
 	}
 
-	int successes = 0;
-
-	for (int i{ 0 }; i < itterations; i++)
-	{
-		unit.pop();
-		successes += unit.getCount() == itterations - (i + 1);
-	}
-
-	if (successes == itterations)
+	if (successes() == itterations)
 	{
 		return std::string();
 	}
 
 	return "objects count pop test failed\n";
+}
+
+int ObjectsCountPopTest::successes()
+{
+	int output = 0;
+
+	for (int i{ 0 }; i < itterations; i++)
+	{
+		unit.pop();
+		output += unit.getCount() == itterations - (i + 1);
+	}
+
+	return output;
 }
